9d2a20c3dd8268598c44eaa554e523d48de4fb49
bb <basharoo.bh@gmail.com>
2025-05-09 15:45:41 +0300

    Cleaning Lawyer List And Search

--- lib/presentation/screens/lawyers_list_screen.dart
+++ lib/presentation/screens/lawyers_list_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation/presentation/widgets/lawyer_item.dart';
 
 import '../../../blocs/lawyer_bloc/lawyer_bloc.dart';
 import '../../../blocs/lawyer_bloc/lawyer_event.dart';
 import '../../../blocs/lawyer_bloc/lawyer_state.dart';
-import '../../../blocs/lawyer_profile_bloc/lawyer_profiel_bloc.dart';
 import '../../../data/models/lawyer_model.dart';
-import 'lawyer_screens/lawyer_profile_screens/lawyer_details_screen.dart';
+import '../widgets/custom_search_bar.dart';
 
 class LawyersListScreen extends StatefulWidget {
   const LawyersListScreen({super.key});
@@ -16,7 +16,6 @@ class LawyersListScreen extends StatefulWidget {
 }
 
 class _LawyersScreenState extends State<LawyersListScreen> {
-  final TextEditingController _searchController = TextEditingController();
   late LawyerBloc bloc;
   List<LawyerModel> _allLawyers = [];
 
@@ -59,7 +58,10 @@ class _LawyersScreenState extends State<LawyersListScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            _buildSearchBar(),
+            CustomSearchBar(
+              hint: 'Search by Lawyer Name',
+              onSearch: _onSearch,
+            ),
             const SizedBox(height: 20),
             Expanded(
               child: BlocBuilder<LawyerBloc, LawyerState>(
@@ -69,8 +71,6 @@ class _LawyersScreenState extends State<LawyersListScreen> {
                   } else if (state is LawyersListLoaded) {
                     _allLawyers = state.lawyersList;
                     return _buildLawyerList(_allLawyers);
-                  } else if (state is LawyerInfoByIdLoaded) {
-                    return _buildLawyerList([state.lawyerModel]);
                   } else if (state is LawyerFail) {
                     return Center(
                       child: Text(
@@ -89,38 +89,14 @@ class _LawyersScreenState extends State<LawyersListScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          bloc.add(GetAllLawyersEvent());
+          bloc.add(
+            GetAllLawyersEvent(),
+          );
         },
         backgroundColor: const Color(0XFF472A0C),
         child: const Icon(
           Icons.refresh,
           color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSearchBar() {
-    return Material(
-      elevation: 5,
-      borderRadius: BorderRadius.circular(30),
-      child: TextField(
-        controller: _searchController,
-        onChanged: _onSearch,
-        decoration: InputDecoration(
-          hintText: 'Search by Lawyer ID...',
-          prefixIcon: const Icon(
-            Icons.search,
-            color: Color(0XFF472A0C),
-          ),
-          filled: true,
-          fillColor: Colors.white,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30),
-            borderSide: BorderSide.none,
-          ),
         ),
       ),
     );
@@ -136,83 +112,11 @@ class _LawyersScreenState extends State<LawyersListScreen> {
       itemCount: lawyers.length,
       separatorBuilder: (_, __) => const SizedBox(height: 12),
       itemBuilder: (context, index) {
-        return _buildLawyerCard(lawyers[index]);
-      },
-    );
-  }
-
-  Widget _buildLawyerCard(LawyerModel lawyer) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => BlocProvider(
-              create: (context) => LawyerProfileBloc(),
-              child: LawyerDetailsScreen(
-                lawyerId: lawyer.id,
-              ),
-            ),
-          ),
+        return LawyerItem(
+          lawyer: lawyers[index],
+          bloc: bloc,
         );
       },
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            20,
-          ),
-        ),
-        elevation: 6,
-        shadowColor: Colors.deepPurple.withOpacity(0.3),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                lawyer.name,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Color(0XFF472A0C),
-                ),
-              ),
-              const SizedBox(height: 8),
-              _buildInfoRow('Experience:', '${lawyer.experienceYears} years'),
-              _buildInfoRow('Specialization:', lawyer.specialization),
-              const SizedBox(height: 10),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: IconButton(
-                  onPressed: () {
-                    bloc.add(DeleteLawyerByIdEvent(lawyerId: lawyer.id));
-                  },
-                  icon: const Icon(Icons.delete, color: Colors.red),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildInfoRow(String title, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: Row(
-        children: [
-          Text(
-            '$title ',
-            style: const TextStyle(fontWeight: FontWeight.w600),
-          ),
-          Expanded(
-            child: Text(
-              value,
-              style: const TextStyle(color: Colors.black87),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/presentation/widgets/lawyer_item.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation/data/models/lawyer_model.dart';
+import 'package:graduation/presentation/widgets/info_row.dart';
+
+import '../../blocs/lawyer_bloc/lawyer_bloc.dart';
+import '../../blocs/lawyer_bloc/lawyer_event.dart';
+import '../../blocs/lawyer_profile_bloc/lawyer_profiel_bloc.dart';
+import '../screens/lawyer_screens/lawyer_profile_screens/lawyer_details_screen.dart';
+
+class LawyerItem extends StatelessWidget {
+  const LawyerItem({super.key, required this.lawyer, required this.bloc});
+  final LawyerModel lawyer;
+  final    LawyerBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => BlocProvider(
+              create: (context) => LawyerProfileBloc(),
+              child: LawyerDetailsScreen(
+                lawyerId: lawyer.id,
+              ),
+            ),
+          ),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+        ),
+        elevation: 6,
+        shadowColor: Colors.deepPurple.withOpacity(0.3),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                lawyer.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Color(0XFF472A0C),
+                ),
+              ),
+              const SizedBox(height: 8),
+              InfoRow(
+                  title: 'Experience:',
+                  value: '${lawyer.experienceYears} years'),
+              InfoRow(title: 'Specialization:', value: lawyer.specialization),
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: IconButton(
+                  onPressed: () {
+                    bloc.add(
+                      DeleteLawyerByIdEvent(lawyerId: lawyer.id),
+                    );
+                  },
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
